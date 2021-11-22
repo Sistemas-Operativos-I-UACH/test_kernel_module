832b79ec40106114a1af8f3dd68c80207d45ff87
Iván Chavero <ichavero@chavero.com.mx>
2021-11-22 14:41:01 -0700

    código inicial de módulo de kernel agregado

---
+++ example_kernel_module/pepito_module.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init  pepito_init(void) {
+    printk(KERN_INFO "Loading pepito module\n");
+    return 0;
+}
+
+static void __exit pepito_exit(void) {
+    printk(KERN_INFO "pepito module successfully unloaded\n");
+}
+
+module_init(pepito_init);
+module_exit(pepito_exit);
