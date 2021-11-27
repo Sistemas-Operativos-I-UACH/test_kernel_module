9f4b7932cb2c1cd0c96e6da8b95a4f5b0e3ec237
LuisHM5 <a338926@uach.mx>
2021-11-27 12:05:32 -0700

    device td28_module

--- td28_kernel_module/td28_module.c
+++ td28_kernel_module/td28_module.c
@@ -1,17 +1,78 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME "td28_module"
 
 MODULE_LICENSE("GPL");
 
-static int __init  td28_init(void) {
-    printk(KERN_INFO "Loading tamales de la 28 kernel module\n");
+static int major;
+
+static int dev_open(struct inode*, struct file*);
+static int dev_release(struct inode*, struct file*);
+static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+
+static struct file_operations fops = {
+   .open = dev_open,
+   .read = dev_read,
+   .write = dev_write,
+   .release = dev_release,
+};
+
+static int __init td28_init(void) {
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if (major < 0) {
+        printk(KERN_ALERT "Tamales de la 28 module load failed\n");
+        return major;
+    }
+
+    printk(KERN_INFO "Tamales de la 28 has been loaded\n");
+
+    printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);
+    printk(KERN_INFO "the driver, create a dev file with\n");
+    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, major);
+    printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+    printk(KERN_INFO "the device file.\n");
+    printk(KERN_INFO "Remove the device file and module when done.\n");
+
     return 0;
 }
 
 static void __exit td28_exit(void) {
-    printk(KERN_INFO "Tamales de la 28 kernel module successfully unloaded\n");
+    printk(KERN_INFO "Tamales de la 28 module successfully unloaded\n");
 }
+
+static int dev_open(struct inode *inodep, struct file *filep) {
+   printk(KERN_INFO "Tamales de la 28 module device opened\n");
+   return 0;
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer,
+                         size_t len, loff_t *offset) {
+
+   printk(KERN_INFO "Sorry, Tamales de la 28 is read only\n");
+   return -EFAULT;
+}
+
+static int dev_release(struct inode *inodep, struct file *filep) {
+   printk(KERN_INFO "Tamales de la 28 device closed\n");
+   return 0;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+    int errors = 0;
+    char *message = "Party rock is in the house tonight, Everybody just have a good time...";
+    int message_len = strlen(message);
+
+    errors = copy_to_user(buffer, message, message_len);
+
+    return errors == 0 ? message_len : -EFAULT;
+}
+
 
 module_init(td28_init);
 module_exit(td28_exit);
