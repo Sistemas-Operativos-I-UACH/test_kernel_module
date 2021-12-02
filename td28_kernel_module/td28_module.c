578f4fa07a9acc014383f67d10755e3037bbeacb
LuisHM5 <a338926@uach.mx>
2021-12-01 18:06:32 -0700

    estructura inicial

--- td28_kernel_module/td28_module.c
+++ td28_kernel_module/td28_module.c
@@ -3,6 +3,8 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
 
 #define DEVICE_NAME "td28_module"
 
@@ -14,6 +16,7 @@ static int dev_open(struct inode*, struct file*);
 static int dev_release(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
 static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+int print_process_list(char *buffer);                                    
 
 static struct file_operations fops = {
    .open = dev_open,
@@ -21,6 +24,10 @@ static struct file_operations fops = {
    .write = dev_write,
    .release = dev_release,
 };
+
+struct task_struct *task;        /*    Structure defined in sched.h for tasks/processes    */
+struct task_struct *task_child;        /*    Structure needed to iterate through task children    */
+struct list_head *list;            /*    Structure needed to iterate through the list in each task->children struct    */
 
 static int __init td28_init(void) {
     major = register_chrdev(0, DEVICE_NAME, &fops);
@@ -38,7 +45,7 @@ static int __init td28_init(void) {
     printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
     printk(KERN_INFO "the device file.\n");
     printk(KERN_INFO "Remove the device file and module when done.\n");
-
+    //print_process_list();
     return 0;
 }
 
@@ -62,17 +69,57 @@ static int dev_release(struct inode *inodep, struct file *filep) {
    printk(KERN_INFO "Tamales de la 28 device closed\n");
    return 0;
 }
+int imprimio=0;
 
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
     int errors = 0;
-    char *message = "Party rock is in the house tonight, Everybody just have a good time...";
-    int message_len = strlen(message);
+    if(imprimio==1)
+        return 0;
 
-    errors = copy_to_user(buffer, message, message_len);
-
-    return errors == 0 ? message_len : -EFAULT;
+    errors =print_process_list(buffer);
+    imprimio=1;
+    return errors; //errors== 0 ? message_len : -EFAULT;
 }
 
+/*    Init Module    */
+
+int print_process_list(char *buffer)                    
+{
+    char message[255];
+    int message_len=0;
+    int errors = 0;
+    /*    good practice to log when loading/removing modules    */
+   printk(KERN_INFO "%s","LOADING MODULE\n");   
+
+    /*    for_each_process() MACRO for iterating through each task in the os located in linux\sched\signal.h    */
+    for_each_process( task ){     
+
+        /*    log parent id/executable name/state    */ 
+
+        sprintf(message, "PARENT PID: %d PROCESS: %s STATE: %ld\n",task->pid, task->comm,task->__state);
+
+        printk(KERN_INFO "\nPARENT PID: %d PROCESS: %s STATE: %ld PRIORITY: %ld NICE:%ld",task->pid, task->comm, task->__state, task->prio, 0);
+        message_len=strlen(message);
+        errors=copy_to_user(buffer, message, message_len); 
+        //imprimio=0;
+         /*    list_for_each MACRO to iterate through task->children    */
+        list_for_each(list, &task->children){                       
+            /*    using list_entry to declare all vars in task_child struct    */
+            task_child = list_entry( list, struct task_struct, sibling );    
+
+            /*    log child of and child pid/name/state    */
+            sprintf(message, "CHILD OF %s[%d] PID: %d PROCESS: %s STATE: %ld \n",task->comm, task->pid, 
+                task_child->pid, task_child->comm,task_child->__state);
+
+            printk(KERN_INFO "\nCHILD OF %s[%d] PID: %d PROCESS: %s STATE: %ld",task->comm, task->pid, 
+                task_child->pid, task_child->comm, task_child->__state);
+        }
+        printk("-----------------------------------------------------"); 
+    }  
+    return message_len;
+
+
+}
 
 module_init(td28_init);
 module_exit(td28_exit);
