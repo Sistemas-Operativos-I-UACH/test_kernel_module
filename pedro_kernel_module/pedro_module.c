d20afabe96eab961520b8893d4e48381b4c832bb
Emilio Salas <a235712@uach.mx>
2021-12-07 12:15:40 -0700

    print process info

--- pedro_kernel_module/pedro_module.c
+++ pedro_kernel_module/pedro_module.c
@@ -3,6 +3,10 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+//#include <sys/sysinfo.h>    //  Struct with total RAM info
 
 #define DEVICE_NAME "pedro_module"
 
@@ -14,6 +18,9 @@ static int dev_open(struct inode*, struct file*);
 static int dev_release(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
 static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+int print_process_list(char *buffer);
+//int sysinfo (struct sysinfo *info); // prototipo funcion
+
 
 static struct file_operations fops = {
     .open = dev_open,
@@ -21,6 +28,11 @@ static struct file_operations fops = {
     .write = dev_write,
     .release = dev_release,
 };
+
+struct task_struct *task;           /*    Structure defined in sched.h for tasks/processes    */
+struct task_struct *task_child;     /*    Structure needed to iterate through task children    */
+struct list_head *list;             /*    Structure needed to iterate through the list in each task->children struct    */
+//struct sysinfo info;
 
 static int __init  pedro_module_init(void) {
     major = register_chrdev(0, DEVICE_NAME, &fops);
@@ -43,6 +55,7 @@ static int __init  pedro_module_init(void) {
 }
 
 static void __exit pedro_exit(void) {
+    unregister_chrdev(major, DEVICE_NAME);
     printk(KERN_INFO "pedro kernel module successfully unloaded\n");
 }
 
@@ -63,10 +76,75 @@ static int dev_release(struct inode *inodep, struct file *filep) {
 
 static ssize_t dev_read(struct file *filep, char *buffer,size_t len, loff_t *offset) {
     int errors = 0;
-    char *message = "never gonna give you up, never gonna let you down...";
-    int message_len = strlen(message);
 
-    errors = copy_to_user(buffer, message, message_len);
+    errors = print_process_list(buffer);
 
+    return 0;
+}
+
+int print_process_list(char *buffer){
+    
+    char proc_string[255];
+    int message_len = 0;
+    int errors = 0;
+    long int rss_memory;
+    long int rss_child;
+    char *process_type; 
+    
+    /*    good practice to log when loading/removing modules    */
+    printk(KERN_INFO "LOADING MODULE\n");       
+
+    /*    for_each_process() MACRO for iterating through each task in the os located in linux\sched\signal.h    */
+    for_each_process(task){
+        
+        get_task_struct(task);
+        
+        if(task->mm){
+            rss_memory = get_mm_rss(task->mm) * 4;
+            process_type = "Usuario";
+        }
+        else {
+            rss_memory = 0;
+            process_type = "Kernel";
+        }
+
+        /*    log parent id/executable name/memory/niceness    */
+        sprintf(proc_string, "\nPARENT PID: %d\nPROCESS: %s\nMEMORIA: %ld kB\nNICENESS: %d", task->pid, task->comm, rss_memory, task_nice(task));
+        printk(KERN_INFO "\nPARENT PID: %d\nPROCESS: %s\nMEMORIA: %ld kB\nNICENESS: %d", task->pid, task->comm, rss_memory, task_nice(task));
+        printk(KERN_INFO "PROCESS TYPE: %s", process_type);
+        message_len = strlen(proc_string);
+        printk(KERN_INFO "Message len: %d\n", message_len);
+        errors = copy_to_user(buffer, proc_string, message_len);
+
+        /*    list_for_each MACRO to iterate through task->children    */
+        list_for_each(list, &task->children){
+            /*    using list_entry to declare all vars in task_child struct    */
+            task_child = list_entry(list, struct task_struct, sibling);
+            get_task_struct(task_child);
+
+            if(task_child->mm){
+                rss_child = get_mm_rss(task_child->mm) * 4;
+                process_type = "Usuario";
+            }
+            else{
+                rss_child = 0;
+                process_type = "Kernel";
+            }
+
+            /*    log child of and child pid/name/memory/niceness    */
+            sprintf(proc_string, "\nCHILD OF %s[%d]\nPID: %d\nPROCESS: %s\nMEMORIA: %ld  kB\nNICENESS: %d", task->comm, task->pid, task_child->pid, task_child->comm, rss_child, task_nice(task_child));
+            printk(KERN_INFO "\nCHILD OF %s[%d]\nPID: %d\nPROCESS: %s\nMEMORIA: %ld  kB\nNICENESS: %d", task->comm, task->pid, task_child->pid, task_child->comm, rss_child, task_nice(task_child));
+            printk(KERN_INFO "PROCESS TYPE: %s", process_type);
+            message_len = strlen(proc_string);
+            errors = copy_to_user(buffer, proc_string, message_len);
+        }
+        
+        printk("-----------------------------------------------------");
+    }
+    //sysinfo(&info);
+    //printk(KERN_INFO "TOTAL MEMORY: %ld", info.totalram/1024);
     return errors == 0 ? message_len : -EFAULT;
 }
+
+module_init(pedro_module_init);
+module_exit(pedro_exit);
