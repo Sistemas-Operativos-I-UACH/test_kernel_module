4068f27dcfad408606cedd3b810d6ba1247c30b3
Alejandro <alexxricoart@gmail.com>
2021-12-07 01:35:52 -0700

    modulo

--- millenials_kernel_module/millenials_module.c
+++ millenials_kernel_module/millenials_module.c
@@ -3,6 +3,9 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
+#include <linux/proc_fs.h>
 
 #define DEVICE_NAME "millenials_module"
 
@@ -14,26 +17,38 @@ static int dev_open(struct inode*, struct file*);
 static int dev_release(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
 static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+int print_process_list(char *buffer);
 
 static struct file_operations fops = {
-    .open = dev_open,
-    .read = dev_read,
-    .write = dev_write,
-    .release = dev_release,
+   .open = dev_open,
+   .read = dev_read,
+   .write = dev_write,
+   .release = dev_release,
 };
-static int __init millenials_init(void) {
+
+struct task_struct *task;        /*    Structure defined in sched.h for tasks/processes    */
+struct task_struct *task_child;        /*    Structure needed to iterate through task children    */
+struct list_head *list;            /*    Structure needed to iterate through the list in each task->children struct    */
+
+
+
+static int __init millenials_module_init(void) {
     major = register_chrdev(0, DEVICE_NAME, &fops);
+
     if (major < 0) {
-        printk(KERN_ALERT "Millenials load failed\n");
+        printk(KERN_ALERT "Super Millenials load failed\n");
         return major;
     }
-    printk(KERN_INFO "Millenials has been loaded\n");
+
+    printk(KERN_INFO "Super Millenials has been loaded\n");
+
     printk(KERN_INFO "I was assigned major number %d. To talk to\n", major);
     printk(KERN_INFO "the driver, create a dev file with\n");
-    printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, major);
+    printk(KERN_INFO "'sudo mknod /dev/%s c %d 0'.\n", DEVICE_NAME, major);
     printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
     printk(KERN_INFO "the device file.\n");
     printk(KERN_INFO "Remove the device file and module when done.\n");
+
     return 0;
 }
 
@@ -42,29 +57,68 @@ static void __exit millenials_exit(void) {
 }
 
 static int dev_open(struct inode *inodep, struct file *filep) {
-    printk(KERN_INFO "Millenials device opened\n");
-    return 0;
+   printk(KERN_INFO "Super Millenials device opened\n");
+   return 0;
 }
 
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
-    printk(KERN_INFO "Millenials is read only\n");
-    return -EFAULT;
+   printk(KERN_INFO "Sorry, Super Millenials is read only\n");
+   return -EFAULT;
 }
 
 static int dev_release(struct inode *inodep, struct file *filep) {
-    printk(KERN_INFO "Millenials device closed\n");
+   printk(KERN_INFO "Super Millenials device closed\n");
+   return 0;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
+    int errors = 0;
+    //char *message = "never gonna give you up, never gonna let you down...";
+    //int message_len = strlen(message);
+
+    //errors = copy_to_user(buffer, message, message_len);
+    errors = print_process_list(buffer);
+
+    //return errors == 0 ? message_len : -EFAULT;
     return 0;
 }
 
-static ssize_t dev_read(struct file *filep, char *buffer,size_t len, loff_t *offset) {
+int print_process_list(char *buffer){
+    char proc_string[255];
+    int message_len = 0;
     int errors = 0;
-    char *message = "never gonna give you up, never gonna let you down...";
-    int message_len = strlen(message);
 
-    errors = copy_to_user(buffer, message, message_len);
+    printk("- - - - - - - - - - - - - - - - - - - - - - -");
+    printk(KERN_INFO "%s","\tLOADING MODULE\n");
+    printk("- - - - - - - - - - - - - - - - - - - - - - -");
 
+    for_each_process( task ){
+        /*    log parent id/executable name/state    */
+        //task->on_cpu task->prio
+        sprintf(proc_string, "\nPARENT PID: %d PROCESS: %s STATE: ", task->pid, task->comm);
+        printk(KERN_INFO "\nPARENT PID: %d PROCESS: %s STATE: ",task->pid, task->comm);
+        message_len = strlen(proc_string);
+        printk(KERN_INFO "Message len: %d", message_len);
+        errors = copy_to_user(buffer, proc_string, message_len);
+
+        /*    list_for_each MACRO to iterate through task->children    */
+        list_for_each(list, &task->children){
+
+            /*    using list_entry to declare all vars in task_child struct    */
+            task_child = list_entry( list, struct task_struct, sibling );
+
+            /*    log child of and child pid/name/state    */
+            sprintf(proc_string, "\nCHILD OF %s[%d] PID: %d PROCESS: %s STATE: ",task->comm, task->pid,
+                task_child->pid, task_child->comm);
+            printk(KERN_INFO "\nCHILD OF %s[%d] PID: %d PROCESS: %s STATE: ",task->comm, task->pid,
+                task_child->pid, task_child->comm);
+            message_len = strlen(proc_string);
+            errors = copy_to_user(buffer, proc_string, message_len);
+        }
+        printk("-----------------------------------------------------");    /*for aesthetics*/
+    }
     return errors == 0 ? message_len : -EFAULT;
 }
 
-module_init(millenials_init);
+module_init(millenials_module_init);
 module_exit(millenials_exit);
